4b671c24e67225ad7f11c4af15fc9b0ae1f35977
Nathan Sodja <sodjanathan@gmail.com>
2022-06-26 15:37:45 +0000

    create server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to node app!");
+});
+
+app.listen(5000, () => {
+  console.log("Server listening on port " + port);
+});
