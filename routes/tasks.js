787d96761b766bf56916eb87fd81958b64666b5c
Nathan Sodja <sodjanathan@gmail.com>
2022-08-14 22:37:38 +0000

    feat: connection attempt

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,10 +16,10 @@ router.route("/:id").get(getSingleTask);
 router.route("/:id").patch(updateSingleTask);
 router.route("/:id").delete(deleteTask);
 
-// app.get("/api/v1/tasks") 	     - get all the task
-// app.post("/api/v1/tasks")		 - create a new task
-// app.get("/api/v1/tasks/:id)       - get a task
-// app.patch("/api/v1/tasks/:id)     - update a task
-// app.delete("/api/v1/tasks/:id")   - delete a task
+// app.get("/api/v1/tasks") 	          - get all the task
+// app.post("/api/v1/tasks")		        - create a new task
+// app.get("/api/v1/tasks/:id)          - get a task
+// app.patch("/api/v1/tasks/:id)        - update a task
+// app.delete("/api/v1/tasks/:id")      - delete a task
 
 module.exports = router;
